3bd5f19b40f76a9cba40d63218efae3add66bcf6
Rendel Abainza <abainzarendel11@gmail.com>
2025-01-21 07:53:44 +0800

    Add Icon

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,11 +1,14 @@
 package application;
-	
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
 
+import java.io.FileInputStream;
+import java.io.File;
 
 public class Main extends Application {
     @Override
@@ -14,12 +17,22 @@ public class Main extends Application {
             // Load the FXML file from the resources folder
             FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/LoadingScreen.fxml"));
             HBox root = loader.load();
-
+            
             // Set the scene with the loaded FXML layout
             Scene scene = new Scene(root, 1200, 780);
 
-            // Set the title and show the stage
-            primaryStage.setTitle("VitalFit");
+            // Set the title
+            primaryStage.setTitle("VitalFit: A Personalized Workout and Activity Tracker");
+
+            // Add the application icon from an external folder
+            File iconFile = new File("Resources/Images/VitalFitIcon.png");
+            if (iconFile.exists()) {
+                primaryStage.getIcons().add(new Image(new FileInputStream(iconFile)));
+            } else {
+                System.out.println("Icon file not found at: " + iconFile.getAbsolutePath());
+            }
+
+            // Show the stage
             primaryStage.setScene(scene);
             primaryStage.show();
             
@@ -27,8 +40,8 @@ public class Main extends Application {
             e.printStackTrace();
         }
     }
-	
-	public static void main(String[] args) {
-		launch(args);
-	}
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
