c49ae99a6ce500504fe1acc2df86c11c39844f93
Rendel Abainza <abainzarendel11@gmail.com>
2025-01-21 00:10:36 +0800

    Added Register - C or Physical Activity Level

--- src/controller/Register_B_Controller.java
+++ src/controller/Register_B_Controller.java
@@ -1,8 +1,14 @@
 package controller;
 
+import java.io.IOException;
+
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
@@ -10,6 +16,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
 public class Register_B_Controller {
 
@@ -66,6 +73,24 @@ public class Register_B_Controller {
     @FXML
     void NextBtn_Clicked(ActionEvent event) {
     	//TODO: Add the values to the database
+    	
+    	
+    	//Change to Register_B
+        try {
+            // Load the Balance Due FXML file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/Register_C.fxml"));
+            Parent Register_CRoot = loader.load();
+
+            // Get the current stage (window) from the event source
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Set the new scene
+            Scene scene = new Scene(Register_CRoot);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @FXML
---
+++ src/controller/Register_C_Controller.java
@@ -0,0 +1,55 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
+public class Register_C_Controller {
+
+    @FXML
+    private Button IntermediateBtn;
+
+    @FXML
+    private Button advanceBtn;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private ImageView background;
+
+    @FXML
+    private Button beginnerBtn;
+
+    @FXML
+    private Button recommendedBtn;
+
+    @FXML
+    void advanceBtn_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void background_Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void beginnerBtn_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void intermediateBtn_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void recommendedBtn_Clicked(ActionEvent event) {
+
+    }
+
+}
