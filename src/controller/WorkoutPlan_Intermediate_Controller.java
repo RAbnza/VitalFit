7630152705eead5326d07d476cf327addd5f7992
Rendel Abainza <abainzarendel11@gmail.com>
2025-01-25 04:11:05 +0800

    Add Controllers

---
+++ src/controller/WorkoutPlan_Intermediate_Controller.java
@@ -0,0 +1,110 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+import javafx.scene.web.WebView;
+
+public class WorkoutPlan_Intermediate_Controller {
+
+    @FXML
+    private Text NameText11;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private ImageView background;
+
+    @FXML
+    private Text dashboardBtn;
+
+    @FXML
+    private Text durationText;
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private Button markAsDoneBtn;
+
+    @FXML
+    private Text numOfSetsText;
+
+    @FXML
+    private ImageView playBtn;
+
+    @FXML
+    private Text profileBtn;
+
+    @FXML
+    private Text progressBtn;
+
+    @FXML
+    private Text resourcesBtn;
+
+    @FXML
+    private Text workoutDayText;
+
+    @FXML
+    private Text workoutDescriptionText;
+
+    @FXML
+    private Text workoutPlanBtn;
+
+    @FXML
+    private Text workoutTitleText;
+
+    @FXML
+    private WebView workoutVideoFrame;
+
+    @FXML
+    void background_Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void dashboardBtn_Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logoutBtn_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void markAsDoneBtn_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void playBtn_Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void profileBtn_Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void progressBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void resourcesBtn_Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void workoutPlanBtn_Clicked(MouseEvent event) {
+
+    }
+
+}
