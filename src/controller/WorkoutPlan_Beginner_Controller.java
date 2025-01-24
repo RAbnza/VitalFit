cd770beb18a7a0ffab605c407a777aeb20928ccf
Fruuity <jbfruto1010@gmail.com>
2025-01-25 05:15:51 +0800

    added button functions in workout beginner

--- src/controller/WorkoutPlan_Beginner_Controller.java
+++ src/controller/WorkoutPlan_Beginner_Controller.java
@@ -15,6 +15,7 @@ import java.time.Period;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -208,11 +209,45 @@ public class WorkoutPlan_Beginner_Controller {
     @FXML
     void dashboardBtn_Clicked(MouseEvent event) {
 
+    	//Change to Dashboard
+        try {
+            // Load the Balance Due FXML file
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/Dashboard.fxml"));
+            Parent DashboardRoot = loader.load();
+
+            // Get the current stage (window) from the event source
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+            // Set the new scene
+            Scene scene = new Scene(DashboardRoot);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    	
     }
 
     @FXML
     void logoutBtn_Clicked(ActionEvent event) {
 
+        // Clear the session
+        SessionManager.getInstance().clearSession();
+
+        // Redirect to Login scene
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/Sign_In.fxml"));
+            Parent loginRoot = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(loginRoot);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    	
     }
 
     @FXML
@@ -302,12 +337,22 @@ public class WorkoutPlan_Beginner_Controller {
         }
     }
 
-
-
-
     @FXML
     void profileBtn_Clicked(MouseEvent event) {
+    	
+    	try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/Profile.fxml"));
+            Parent profileRoot = loader.load();
 
+            // Switch scene
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(profileRoot);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    	
     }
 
     @FXML
@@ -318,6 +363,19 @@ public class WorkoutPlan_Beginner_Controller {
     @FXML
     void resourcesBtn_Clicked(MouseEvent event) {
 
+    	try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/layouts/Resources.fxml"));
+            Parent ResourcesRoot = loader.load();
+
+            // Switch scene
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            Scene scene = new Scene(ResourcesRoot);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    	
     }
 
     @FXML
