f1cee31e29737d7cbe8a398427e4c32538e52446
Fruuity <jbfruto1010@gmail.com>
2025-01-24 17:34:23 +0800

    Revert "Logout function"
    This reverts commit a55b910d06499d7d3d0670f518749f7bae6ef5de.

--- src/controller/SessionManager.java
+++ src/controller/SessionManager.java
@@ -4,10 +4,10 @@ public class SessionManager {
     private static SessionManager instance;
     private String username;
 
-    // Private constructor to enforce singleton pattern
-    private SessionManager() {}
+    private SessionManager() {
+        // Private constructor to prevent instantiation
+    }
 
-    // Get the single instance of SessionManager
     public static SessionManager getInstance() {
         if (instance == null) {
             instance = new SessionManager();
@@ -15,19 +15,11 @@ public class SessionManager {
         return instance;
     }
 
-    // Set the username for the session
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    // Get the username for the session
     public String getUsername() {
         return username;
     }
 
-    // Clear session data
-    public void clearSession() {
-        username = null;
+    public void setUsername(String username) {
+        this.username = username;
     }
 }
-
