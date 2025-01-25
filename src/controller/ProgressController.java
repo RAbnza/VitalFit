2e380074fcbdd2e2e831bbec4750a3dcb3600419
Rendel Abainza <abainzarendel11@gmail.com>
2025-01-25 11:18:45 +0800

    Add Controller to Progress

---
+++ src/controller/ProgressController.java
@@ -0,0 +1,97 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
+
+public class ProgressController {
+
+    @FXML
+    private Text NameText;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private ImageView background;
+
+    @FXML
+    private Text currentDay;
+
+    @FXML
+    private Text currentStatusText;
+
+    @FXML
+    private Text currentWorkoutText;
+
+    @FXML
+    private Text dashboardBtn;
+
+    @FXML
+    private Text finishedWorkoutText;
+
+    @FXML
+    private Text heightText;
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private Text profileBtn;
+
+    @FXML
+    private ProgressBar progressBar;
+
+    @FXML
+    private Text progressBtn;
+
+    @FXML
+    private Text resourcesBtn;
+
+    @FXML
+    private Text weightText;
+
+    @FXML
+    private Text workoutPlanBtn;
+
+    @FXML
+    void background_Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void dashboardBtn_Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void logoutBtn_Clicked(ActionEvent event) {
+
+    }
+
+    @FXML
+    void profileBtn_Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void progressBtn(MouseEvent event) {
+
+    }
+
+    @FXML
+    void resourcesBtn_Clicked(MouseEvent event) {
+
+    }
+
+    @FXML
+    void workoutPlanBtn_Clicked(MouseEvent event) {
+
+    }
+
+}
